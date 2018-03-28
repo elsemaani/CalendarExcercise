6e80a3062326a6f30972e647a872024baf107f67
elsemaani <hatoviejo@gmail.com>
2018-03-28 10:18:24 -0600

    chore: Added labels and inputs.

--- calendar-app/src/components/Calendar.jsx
+++ calendar-app/src/components/Calendar.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './calendar.css';
 
 class Calendar extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            startDate: new Date(),
+            days: 15,
+            country: ''
         }
     }
 
@@ -22,6 +26,18 @@ class Calendar extends Component {
 
         return <div>
             <h1>Calendar Component</h1>
+            <div className="input-container">
+                <div className="left-label">{'Start Date:'}</div>
+                <input type="date" value={this.state.date} />
+            </div>
+            <div className="input-container">
+                <div className="left-label">{'Number of days:'}</div>
+                <input type="text" value={this.state.days} />
+            </div>
+            <div className="input-container">
+                <div className="left-label">{'Country Code:'}</div>
+                <input type="text" value={this.state.country} />
+            </div>
         </div>
     }
 }
