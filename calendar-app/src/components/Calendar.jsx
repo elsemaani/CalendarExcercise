13d2b10ad0582f5e8948a9c62f96bd8b3ac8b32c
elsemaani <hatoviejo@gmail.com>
2018-03-28 09:51:51 -0600

    chore: Cleanup and new components folder.

---
+++ calendar-app/src/components/Calendar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Calendar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    static defaultProps = {
+    };
+
+    static propTypes = {
+    };
+
+    componentDidMount() {
+    }
+
+    render() {
+
+        return <div>
+            <h1>Calendar Component</h1>
+        </div>
+    }
+}
+
+export default Calendar;
